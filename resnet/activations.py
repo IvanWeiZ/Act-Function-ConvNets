f4a509f59a75ef4a074ab0353d0384e72ad09298
Wei Zheng <ivan.zheng1201@gmail.com>
2017-12-15 04:29:13 -0500

    123

--- resnet/activations.py
+++ resnet/activations.py
@@ -60,7 +60,7 @@ def _new_shape(op):
 @function.Defun(shape_func=_new_shape, func_name="new_grad", noinline=True)
 def _new_grad(features, grad):
   """Gradient of new function defined below."""
-  activation_grad=tf.minimum(features,features*tf.exp(-tf.abs(features)))
+  activation_grad=tf.minimum(features*tf.exp(-tf.abs(features)),features)
   return grad * activation_grad
 
 @function.Defun(
