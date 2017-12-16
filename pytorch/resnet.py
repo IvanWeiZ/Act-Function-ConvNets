ee6e899f9dcd8393679fa8008a2cf63b5038ec94
Wei Zheng <ivan.zheng1201@gmail.com>
2017-12-16 06:28:32 -0500

    typo

--- pytorch/resnet.py
+++ pytorch/resnet.py
@@ -23,7 +23,7 @@ class New(Module):
         return self.__class__.__name__ + ' ()'
 
 def new(input):
-    return (input+F.relu(x * torch.exp(-torch.abs(input)) - input))
+    return (input + F.relu(input * torch.exp(-torch.abs(input)) - input))
 
 def resnet(depth, width, num_classes,activation):
     assert (depth - 4) % 6 == 0, 'depth should be 6n+4'
